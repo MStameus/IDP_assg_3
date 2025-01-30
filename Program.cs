5206efc74eb47706678b3826333c32d5b30c3363
MStameus <morris.stameus@hotmail.se>
2025-01-30 04:30:04 +0100

    Lade till class för grundämne samt innstantsierade syre järn och guld

--- Program.cs
+++ Program.cs
@@ -1,10 +1,43 @@
-﻿namespace IDP_assg_3
+﻿using System.Xml;
+
+namespace IDP_assg_3
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+        public class Grundämne
         {
-            Console.WriteLine("Hello, World!");
+            public string Namn;
+            public int Atomnummer;
+            public string Typ;
+            public double Smältpunkt;
+            public double Kokpunkt;
+
+            public Grundämne(string namn, int atomnummer, string typ, double smältpunkt, double kokpunkt)
+            {
+                Namn = namn;
+                Atomnummer = atomnummer;
+                Typ = typ;
+                Smältpunkt = smältpunkt;
+                Kokpunkt = kokpunkt;
+            }
+
+            public void Print()
+            {
+                Console.WriteLine($"Grundämne: {Namn}");
+                Console.WriteLine($"  Atomnummer: {Atomnummer}");
+                Console.WriteLine($"  Typ: {Typ}");
+                Console.WriteLine($"  Smältpunkt: {Smältpunkt} K");
+                Console.WriteLine($"  Kokpunkt: {Kokpunkt} K");
+                Console.WriteLine();
+            }
+
+        }
+        public static void Main(string[] args)
+        {
+            Grundämne syre = new("Syre", 8, "Ickemetall", 54.36, 90.188);
+            Grundämne järn = new("Järn", 26, "Metall", 1811, 3134);
+            Grundämne guld = new("Guld", 79, "Metall", 1337.33, 3243);
+
         }
     }
 }
