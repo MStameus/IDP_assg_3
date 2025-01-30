7bb43e55e11c3ebba6452af7577ce816cb160376
MStameus <morris.stameus@hotmail.se>
2025-01-30 04:32:45 +0100

    array för syre,järn och guld samt nya väte brom och kvicksilver

--- Program.cs
+++ Program.cs
@@ -42,6 +42,15 @@ namespace IDP_assg_3
             järn.Print();
             guld.Print();
 
+            Grundämne[] grundämnenarr = new Grundämne[6];
+
+            grundämnenarr[0] = syre;
+            grundämnenarr[1] = järn;
+            grundämnenarr[2] = guld;
+            grundämnenarr[3] = new Grundämne("Väte", 1, "Ickemetall", 13.99, 20.271);
+            grundämnenarr[4] = new Grundämne("Brom", 35, "Ickemetall", 265.8, 332.0);
+            grundämnenarr[5] = new Grundämne("Kvicksilver", 80, "Metall", 234.3210, 629.88);
+
         }
     }
 }
